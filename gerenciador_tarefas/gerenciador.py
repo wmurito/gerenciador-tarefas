1b624adc19e9c96f23bc5fa579eb996056b7a781
wmurito <wellington_murito@hotmail.com>
2022-05-19 23:09:10 -0300

    Adiciona funcionalidade de criar tarefas

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,29 +1,66 @@
-from fastapi import FastAPI
+from enum import Enum
+from uuid import UUID, uuid4
 
-TAREFAS = [
-    {
-        "id": "1",
-        "titulo": "fazer compras",
-        "descrição": "comprar leite e ovos",
-        "estado": "não finalizado",
-    },
-    {
-        "id": "2",
-        "titulo": "levar o cachorro para tosar",
-        "descrição": "está muito peludo",
-        "estado": "não finalizado",
-    },
-    {
-        "id": "3",
-        "titulo": "lavar roupas",
-        "descrição": "estão sujas",
-        "estado": "não finalizado",
-    },
-]
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel, constr
 
 app = FastAPI()
+
+
+class EstadosPossiveis(str, Enum):
+    finalizado = "finalizado"
+    nao_finalizado = "não finalizado"
+
+
+class TarefaEntrada(BaseModel):
+    titulo: constr(min_length=3, max_length=50)
+    descricao: constr(max_length=140)
+    estado: EstadosPossiveis = EstadosPossiveis.nao_finalizado
+
+
+class Tarefa(TarefaEntrada):
+    id: UUID
+
+
+TAREFAS = []
 
 
 @app.get("/tarefas")
 def listar():
     return TAREFAS
+
+
+@app.post(
+    "/tarefas", response_model=Tarefa, status_code=status.HTTP_201_CREATED
+)
+def criar(tarefa: TarefaEntrada):
+    nova_tarefa = tarefa.dict()
+    nova_tarefa.update({"id": uuid4()})
+    TAREFAS.append(nova_tarefa)
+    return nova_tarefa
+
+
+@app.get("/tarefas/{id}", response_model=Tarefa)
+def detalhar(id: UUID):
+    for tarefa in TAREFAS:
+        if tarefa["id"] == id:
+            return tarefa
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.put("/tarefas/{id}", response_model=Tarefa)
+def atualizar(id: UUID, tarefa: TarefaEntrada):
+    for tarefa_atual in TAREFAS:
+        if tarefa_atual["id"] == id:
+            tarefa_atual.update(tarefa.dict())
+            return tarefa_atual
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.delete("/tarefas/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def deletar(id: UUID):
+    for tarefa in TAREFAS:
+        if tarefa["id"] == id:
+            TAREFAS.remove(tarefa)
+            return tarefa
+    return {"erro": "Tarefa não encontrada"}
